5208eff6b56177f8568396b857aa981212dd79aa
badzlan <badzlandhabith05@gmail.com>
2023-05-15 22:42:21 +0700

    get book by id

--- src/handler.js
+++ src/handler.js
@@ -124,4 +124,25 @@ const getAllBooksHandler = (request, h) => {
    return response;
 };
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+   const { id } = request.params;
+   const book = books.filter((b) => b.id === id)[0];
+
+   if (book !== undefined) {
+      return {
+         status: "success",
+         data: {
+            book,
+         },
+      };
+   }
+
+   const response = h.response({
+      status: "fail",
+      message: "Buku tidak ditemukan",
+   });
+   response.code(404);
+   return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addBookHandler, getAllBooksHandler } = require("./handler");
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler } = require("./handler");
 
 const routes = [
    {
@@ -10,6 +10,11 @@ const routes = [
       method: "GET",
       path: "/books",
       handler: getAllBooksHandler,
+   },
+   {
+      method: "GET",
+      path: "/books/{id}",
+      handler: getBookByIdHandler,
    },
 ];
 
