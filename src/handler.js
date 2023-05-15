b78849ef62bee6d4ba78d516fe7f03c13f855f05
badzlan <badzlandhabith05@gmail.com>
2023-05-15 22:27:28 +0700

    filter query param

--- src/handler.js
+++ src/handler.js
@@ -12,6 +12,7 @@ const addBookHandler = (request, h) => {
       response.code(400);
       return response;
    }
+
    if (readPage > pageCount) {
       const response = h.response({
          status: "fail",
@@ -59,6 +60,56 @@ const addBookHandler = (request, h) => {
 };
 
 const getAllBooksHandler = (request, h) => {
+   const { name, reading, finished } = request.query;
+
+   if (name !== undefined) {
+      const BooksName = books.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+      const response = h.response({
+         status: "success",
+         data: {
+            books: BooksName.map((book) => ({
+               id: book.id,
+               name: book.name,
+               publisher: book.publisher,
+            })),
+         },
+      });
+      response.code(200);
+      return response;
+   }
+
+   if (reading !== undefined) {
+      const BooksReading = books.filter((book) => Number(book.reading) === Number(reading));
+      const response = h.response({
+         status: "success",
+         data: {
+            books: BooksReading.map((book) => ({
+               id: book.id,
+               name: book.name,
+               publisher: book.publisher,
+            })),
+         },
+      });
+      response.code(200);
+      return response;
+   }
+
+   if (finished !== undefined) {
+      const BooksFinished = books.filter((book) => book.finished == finished);
+      const response = h.response({
+         status: "success",
+         data: {
+            books: BooksFinished.map((book) => ({
+               id: book.id,
+               name: book.name,
+               publisher: book.publisher,
+            })),
+         },
+      });
+      response.code(200);
+      return response;
+   }
+
    const response = h.response({
       status: "success",
       data: {
