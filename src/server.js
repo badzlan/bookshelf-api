d716ac06486cf6fcd054ab455d1ef5efe8ab9c38
badzlan <badzlandhabith05@gmail.com>
2023-05-15 21:47:23 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
    const server = Hapi.server({
       port: 9000,
       host: "localhost",
+      routes: {
+         cors: {
+            origin: ["*"],
+         },
+      },
    });
 
    server.route(routes);
